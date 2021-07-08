004a9dda073657ff62547b12a0a4c43eb0fcbf14
Nestor Cortina <junior110120@gmail.com>
2021-07-08 06:01:40 -0500

    chore: jest setup

---
+++ jest.config.ts
@@ -0,0 +1,27 @@
+import type { Config } from '@jest/types'
+import { pathsToModuleNameMapper } from 'ts-jest/utils'
+const {
+  compilerOptions: { paths: tsconfigPaths }
+} = require('./tsconfig.json')
+
+const paths = {
+  ...pathsToModuleNameMapper(tsconfigPaths)
+}
+Object.assign(
+  paths,
+  ...Object.keys(paths).map((key) => ({
+    [key]: `<rootDir>/src/${paths[key]}`
+  }))
+)
+
+const config: Config.InitialOptions = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  roots: ['<rootDir>/src'],
+  collectCoverageFrom: ['<rootDir>/src/**/*.ts', '!**/node_modules/**'],
+  coverageDirectory: 'coverage',
+  coverageProvider: 'v8',
+  moduleNameMapper: paths,
+  testMatch: ['**/*.spec.ts']
+}
+export default config
