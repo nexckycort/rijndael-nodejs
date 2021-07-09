b81d6bf1b2711e05d091fe932cfc5f63c2229c1b
Nestor Cortina <junior110120@gmail.com>
2021-07-08 22:02:32 -0500

    ci: add package husky and setup

--- src/example.ts
+++
@@ -1,9 +0,0 @@
-import rijndael from 'rijndael'
-
-const saltBytes = Buffer.from('salttest', 'ascii')
-const key = 'keytest'
-const password = 'test'
-
-const encryptRijndael = rijndael(saltBytes, key, password)
-
-console.log(encryptRijndael)
